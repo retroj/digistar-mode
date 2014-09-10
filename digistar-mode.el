5d19820d53e13556f512d054a247234692216eea
John Foerch <jjfoerch@earthlink.net>
2014-09-10 10:24:20 -0400

    Commentary: installation instructions

--- digistar-mode.el
+++ digistar-mode.el
@@ -23,7 +23,13 @@
 ;;; Commentary:
 
 ;; This package provides digistar-mode, a major mode for editing Digistar
-;; scripts.
+;; scripts.  If installed via elpa, the auto-mode-list entry for this mode
+;; will be setup automatically.  If installed manually, use a snippet like
+;; the following to set it up:
+;;
+;;     (when (locate-library "digistar-mode")
+;;      (add-to-list 'auto-mode-alist '("\\.ds\\'" . digistar-mode)))
+;;
 
 ;;; Code:
 
