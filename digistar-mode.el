0dcde58ec6e473042e55d4f283b223554546de5b
John Foerch <jjfoerch@earthlink.net>
2014-10-21 19:46:56 -0400

    version 0.4

--- digistar-mode.el
+++ digistar-mode.el
@@ -3,8 +3,8 @@
 ;; Copyright (C) 2014  John Foerch <jjfoerch@earthlink.net>
 
 ;; Author: John Foerch <jjfoerch@earthlink.net>
-;; Version: 0.3
-;; Date: 2014-09-13
+;; Version: 0.4
+;; Date: 2014-10-21
 ;; Keywords: languages
 
 ;; This program is free software; you can redistribute it and/or
