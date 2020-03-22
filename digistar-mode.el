8b350b7a143219b3f927cb3a1aeb16a299363f05
John Foerch <jjfoerch@gmail.com>
2020-03-22 17:09:13 -0400

    version 0.6.1

--- digistar-mode.el
+++ digistar-mode.el
@@ -3,8 +3,8 @@
 ;; Copyright (C) 2014-2020  John Foerch <jjfoerch@earthlink.net>
 
 ;; Author: John Foerch <jjfoerch@earthlink.net>
-;; Version: 0.6.0
-;; Date: 2020-03-21
+;; Version: 0.6.1
+;; Date: 2020-03-22
 ;; Keywords: languages
 
 ;; This program is free software; you can redistribute it and/or
